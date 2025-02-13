2260a9b603996b321e7c79f4714d94ec64562d13
rKmasterworks <32278144+rKmasterworks@users.noreply.github.com>
2025-02-13 10:21:04 +0100

    url config, map / to home_page view

--- lists/tests.py
+++ lists/tests.py
@@ -7,9 +7,7 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
+        response = self.client.get("/")
         self.assertIn("<title>To-Do lists</title>", html)
         self.assertTrue(html.startswith("<html>"))
         self.assertTrue(html.endswith("</html>"))
