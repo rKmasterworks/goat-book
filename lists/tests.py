4010bd3212c57fbe67f0ba5afa7e65cf94ea41b2
rKmasterworks <32278144+rKmasterworks@users.noreply.github.com>
2025-02-13 10:25:10 +0100

    Correction on tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -8,6 +8,6 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
+        self.assertIn("<title>To-Do lists</title>", )
+        self.assertTrue(response("<html>"))
+        self.assertTrue(response("</html>"))
